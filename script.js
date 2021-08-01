5624c5cee586414e25d701c6e2c11f40f7935963
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-07-31 22:43:59 -0500

    trying to get javascript new card button to work with a modal

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Retrieve elements from DOM
+// Get page elements from DOM
+
+// Create New Card Modal with Get Elements and Event Listeners
+var createCardModal = document.getElementById('createCardModal');
+
+createCardModal.addEventListener('show.bs.modal', function (event) {
+    var createCardButton = document.getElementById('createCardButton');
+    createCardButton = event.relatedTarget;
+})
