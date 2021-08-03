6331dcdff4a8bdb30861e0c9a39f36993126edcf
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-08-03 11:56:59 -0500

    added content and adjusted button colors and function

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// Create variables and constants
-var createCardModal = document.getElementById('createCardModal');
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+// Create variables and constants, DOM elements
+var createCardModal = document.getElementById("createCardModal");
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
+  return new bootstrap.Popover(popoverTriggerEl);
 });
 
 // Create New Card Modal with Get Elements and Event Listeners
-createCardModal.addEventListener('show.bs.modal', function (event) {
-    var createCardButton = document.getElementById('createCardButton');
-    createCardButton = event.relatedTarget;
-})
-
-// Retrieve flashcard DOM elements
+createCardModal.addEventListener("show.bs.modal", function (event) {
+  var createCardButton = document.getElementById("createCardButton");
+  createCardButton = event.relatedTarget;
+});
 
 // Create class for new flashcards
 class flashCard {
-    constructor(cardTitle,cardText,answer) {
-        this.cardTitle = cardTitle;
-        this.cardText = cardText;
-        this.answer = answer;
-    }
+  constructor(cardTitle, cardText, answer) {
+    this.cardTitle = cardTitle;
+    this.cardText = cardText;
+    this.answer = answer;
+  }
 }
