a7a83ab12ca0efc64b229ca5afc5564d6edbd662
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-08-03 11:04:15 -0500

    added popover for answers to cards

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-
+// Create variables and constants
+var createCardModal = document.getElementById('createCardModal');
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+});
 
 // Create New Card Modal with Get Elements and Event Listeners
-var createCardModal = document.getElementById('createCardModal');
-
 createCardModal.addEventListener('show.bs.modal', function (event) {
     var createCardButton = document.getElementById('createCardButton');
     createCardButton = event.relatedTarget;
 })
+
+// Retrieve flashcard DOM elements
+
+// Create class for new flashcards
+class flashCard {
+    constructor(cardTitle,cardText,answer) {
+        this.cardTitle = cardTitle;
+        this.cardText = cardText;
+        this.answer = answer;
+    }
+}
